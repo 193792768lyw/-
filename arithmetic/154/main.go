66a0096c38dd3e86a330c151d6ba382867b60958
yaowuliu <193792lyw$>
2021-04-09 17:36:06 +0800

    154. 寻找旋转排序数组中的最小值 II 2020-04-09

--- arithmetic/154/main.go
+++ arithmetic/154/main.go
@@ -2,8 +2,25 @@ package main
 
 import "fmt"
 
+type name struct {
+	b  int
+	ma map[int]int
+}
+
+/*
+测试结构体内部引用的数据传递时是否会完全copy
+*/
 func main() {
-	fmt.Println(findMin([]int{2, 2, 2, 0, 1}))
+	bb := name{4, map[int]int{}}
+	ch(bb)
+	fmt.Println(bb)
+	//fmt.Println(findMin([]int{2, 2, 2, 0, 1}))
+}
+
+func ch(name2 interface{}) {
+	v := name2.(name)
+	v.ma[9] = 99
+	v.b = 88
 }
 
 // 154. 寻找旋转排序数组中的最小值 II
---
+++ article/article.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+
+}
+
+/*
+四种常见的 POST 提交数据方式（application/x-www-form-urlencoded，multipart/form-data，application/json，text/xml）
+https://blog.csdn.net/xiao__jia__jia/article/details/79357274
+*/
