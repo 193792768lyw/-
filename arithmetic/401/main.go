f2c2b4cc029376936554acd486bb6232f687a774
yaowuliu <193792lyw$>
2021-06-23 09:40:26 +0800

    剑指 Offer 15. 二进制中1的个数 2021-06-23

--- arithmetic/401/main.go
+++ arithmetic/401/main.go
@@ -3,9 +3,29 @@ package main
 import (
 	"fmt"
 	"math/bits"
+	"net"
 )
 
 func main() {
+	netInterfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println("net.Interfaces failed, err:", err.Error())
+	}
+
+	for i := 0; i < len(netInterfaces); i++ {
+		if (netInterfaces[i].Flags & net.FlagUp) != 0 {
+			addrs, _ := netInterfaces[i].Addrs()
+
+			for _, address := range addrs {
+				if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+					if ipnet.IP.To4() != nil {
+						fmt.Println(ipnet.IP.String())
+						break
+					}
+				}
+			}
+		}
+	}
 
 }
 func readBinaryWatch1(turnedOn int) (ans []string) {
