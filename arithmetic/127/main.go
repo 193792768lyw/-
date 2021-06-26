70a0cf1bfeb513b0f239e80132e339ed377be165
刘耀武 <liu@email.com>
2021-06-26 17:13:02 +0800

    752. 打开转盘锁2021-06-20

--- arithmetic/127/main.go
+++ arithmetic/127/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Println(ladderLength("ymain", "oecij", []string{"ymann", "yycrj", "oecij", "ymcnj", "yzcrj", "yycij", "xecij", "yecij", "ymanj", "yzcnj", "ymain"}))
+	fmt.Println(ladderLength("hit", "cog", []string{"hot", "dot", "dog", "lot", "log", "cog"}))
 }
 func ladderLength1(beginWord string, endWord string, wordList []string) int {
 	wordId := map[string]int{}
@@ -65,7 +65,7 @@ func ladderLength1(beginWord string, endWord string, wordList []string) int {
 	return 0
 }
 
-func ladderLength(beginWord string, endWord string, wordList []string) int {
+func ladderLength2(beginWord string, endWord string, wordList []string) int {
 	dead := map[string]bool{}
 	for _, s := range wordList {
 		dead[s] = true
@@ -113,6 +113,87 @@ func ladderLength(beginWord string, endWord string, wordList []string) int {
 				return p.step + 1 + 1
 			}
 			q = append(q, pair{nxt, p.step + 1})
+		}
+	}
+	return 0
+}
+
+func ladderLength(beginWord string, endWord string, wordList []string) int {
+	wordId := map[string]int{}
+	graph := [][]int{}
+	addWord := func(word string) int {
+		id, has := wordId[word]
+		if !has {
+			id = len(wordId)
+			wordId[word] = id
+			graph = append(graph, []int{})
+		}
+		return id
+	}
+	addEdge := func(word string) int {
+		id1 := addWord(word)
+		s := []byte(word)
+		for i, b := range s {
+			s[i] = '*'
+			id2 := addWord(string(s))
+			graph[id1] = append(graph[id1], id2)
+			graph[id2] = append(graph[id2], id1)
+			s[i] = b
+		}
+		return id1
+	}
+
+	for _, word := range wordList {
+		addEdge(word)
+	}
+	beginId := addEdge(beginWord)
+	endId, has := wordId[endWord]
+	if !has {
+		return 0
+	}
+
+	const inf int = math.MaxInt64
+	distBegin := make([]int, len(wordId))
+	for i := range distBegin {
+		distBegin[i] = inf
+	}
+	distBegin[beginId] = 0
+	queueBegin := []int{beginId}
+
+	distEnd := make([]int, len(wordId))
+	for i := range distEnd {
+		distEnd[i] = inf
+	}
+	distEnd[endId] = 0
+	queueEnd := []int{endId}
+
+	for len(queueBegin) > 0 && len(queueEnd) > 0 {
+		q := queueBegin
+		queueBegin = nil
+		for _, v := range q {
+			if distEnd[v] < inf {
+				return (distBegin[v]+distEnd[v])/2 + 1
+			}
+			for _, w := range graph[v] {
+				if distBegin[w] == inf {
+					distBegin[w] = distBegin[v] + 1
+					queueBegin = append(queueBegin, w)
+				}
+			}
+		}
+
+		q = queueEnd
+		queueEnd = nil
+		for _, v := range q {
+			if distBegin[v] < inf {
+				return (distBegin[v]+distEnd[v])/2 + 1
+			}
+			for _, w := range graph[v] {
+				if distEnd[w] == inf {
+					distEnd[w] = distEnd[v] + 1
+					queueEnd = append(queueEnd, w)
+				}
+			}
 		}
 	}
 	return 0
